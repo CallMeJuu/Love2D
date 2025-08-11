f5c611132b108117bb2b3097f5afe0d9d62f50e5
CallMeJuu <justin.kobiolka@outlook.de>
2025-08-11 23:45:42 +0200

    Initial Setup - #1

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title = "Love2D Game"
+    t.window.width = 1920
+    t.window.height = 1080
+    t.window.resizable = true
+end
---
+++ main.lua
@@ -0,0 +1,15 @@
+
+local gamestate = require "gamestate"
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
