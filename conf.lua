57f7096c161c31bb3b05ab05d179e2b7349ca52b
CallMeJuu <justin.kobiolka@outlook.de>
2025-08-26 02:34:43 +0200

    Revamp Setup - #1

--- conf.lua
+++ conf.lua
@@ -2,5 +2,7 @@ function love.conf(t)
     t.window.title = "Love2D Game"
     t.window.width = 1920
     t.window.height = 1080
+    t.window.minwidth = 1280
+    t.window.minheight = 720
     t.window.resizable = true
 end
